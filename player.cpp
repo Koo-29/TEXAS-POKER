039e4358d122703069646f2f6284ceb82b550be4
Alfred Chin <alfredchin2005@gmail.com>
2025-01-09 22:15:49 +0800

    Add deck and player class

---
+++ player.cpp
@@ -0,0 +1,31 @@
+#include "include/player.h"
+
+// constructor
+player::player(string inputName, int inputPot)
+{
+    name = inputName;
+    pot = inputPot;
+}
+
+// setter
+void player::set_hand(string getHand[])
+{
+    hand[0] = getHand[0];
+    hand[1] = getHand[1];
+}
+
+// getter
+string player::get_name()
+{
+    return name;
+}
+
+string player::get_hand(int card)
+{
+    return hand[card];
+}
+
+int player::get_pot()
+{
+    return pot;
+}
