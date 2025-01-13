19b4c74323c17ec40f1e2667a42bcc84a0b20021
Alfred Chin <alfredchin2005@gmail.com>
2025-01-13 18:05:15 +0800

    Create main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "include/player_action.h"
+#include "include/poker_hand.h"
+#include <iomanip>
+
+void initialScreen()
+{
+    cout << setfill('*') << setw(30) << "Welcome to texas poker" << setw(8) << " ";
+}
+
+int main()
+{
+    initialScreen();
+}
